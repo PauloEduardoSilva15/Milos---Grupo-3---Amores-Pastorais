0dfd9f08f096983a69424d234ed9f5d97b3cf38f
PauloEduardoSilva15 <pauloeduardob75@gmail.com>
2025-09-09 16:59:37 -0300

    refazendo erros

--- Milos - Grupo 3/main.c
+++ Milos - Grupo 3/main.c
@@ -16,6 +16,9 @@ int main() {
 
 	bool done = false; // Verifica se o jogo está rodando
 
+	int gravidade = 10;
+
+
 	ALLEGRO_EVENT_QUEUE* events = al_create_event_queue(); // Evento Principal
 	al_register_event_source(events, al_get_keyboard_event_source());
 
@@ -27,6 +30,9 @@ int main() {
 
 			if (ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE) done = true; // O Loop Acaba quando pressiona o ESC
 		}
+
+
+
 
 		al_flip_display();
 		al_clear_to_color(al_map_rgb(0, 0, 0)); // Limpa a Tela
