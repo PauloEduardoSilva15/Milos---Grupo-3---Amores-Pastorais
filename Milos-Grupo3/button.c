ffa7acf3ba7561170a07b68732b6c2b4476ba1a5
PauloEduardoSilva15 <pauloeduardob75@gmail.com>
2025-10-05 19:24:03 -0300

    versão  Alfa 0.0.7

--- Milos-Grupo3/button.c
+++ Milos-Grupo3/button.c
@@ -7,7 +7,9 @@ button newButton(int x, int y, char text[], ALLEGRO_FONT * font) {
 	b.y = y;
 	b.w = BUTTON_X;
 	b.h = QUAD_SIZE;
-	strcpy(b.text, text);
+	strcpy_s(b.text, sizeof(b.text), text);// Windows
+	//strcpy(b.text, text);
+
 	//b.text = text; // Garantir que a string termina com null
 	b.font = font;
 	b.color = BUTTON_COLOR_NORMAL;
