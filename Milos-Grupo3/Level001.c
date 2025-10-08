9560186b700af1e1dc4f9250a21004fa706ef149
PauloEduardoSilva15 <pauloeduardob75@gmail.com>
2025-10-08 13:28:09 -0300

    Tirando o CRT_WARNING

--- Milos-Grupo3/Level001.c
+++ Milos-Grupo3/Level001.c
@@ -3,11 +3,11 @@
 
 levelI Level_I_load(){
     levelI l;
-    l.tileset = load_tileset("Milos-Grupo3/imgs/tilesets/Tileset_Map_001.png", QUAD_SIZE, QUAD_SIZE);
-    l.map = load_tilemap("Milos-Grupo3/maps/001/map001.txt");
+    //l.tileset = load_tileset("Milos-Grupo3/imgs/tilesets/Tileset_Map_001.png", QUAD_SIZE, QUAD_SIZE);
+    //l.map = load_tilemap("Milos-Grupo3/maps/001/map001.txt");
     
-    //l.tileset = load_tileset("./imgs/tilesets/tileset001.png", QUAD_SIZE, QUAD_SIZE);
-    //l.map = load_tilemap("./map001.txt");
+    l.tileset = load_tileset("./imgs/tilesets/Tileset_Map_001.png", QUAD_SIZE, QUAD_SIZE);
+    l.map = load_tilemap("./maps/001/map001.txt");
     l.player = playerLoad();
     l.cameraX = 0;
     l.cameraY = 0;
