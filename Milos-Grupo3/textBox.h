5ea9fd91d8c1c9f1423412b49f3fd9f26ebb52dc
PauloEduardoSilva15 <pauloeduardob75@gmail.com>
2025-10-12 12:58:11 -0300

    PegandoArquivosDaMaster

---
+++ Milos-Grupo3/textBox.h
@@ -0,0 +1,26 @@
+#ifndef TEXTBOX_H
+#define TEXTBOX_H
+
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <stdbool.h>
+#include "gameConstants.h"
+
+
+typedef struct {
+	ALLEGRO_FONT* font;
+	int x, y;
+	int width, height;
+	int dialogueOption;
+	char personagem[16];
+	char texto[50];
+	ALLEGRO_COLOR boxColor;
+}textBox;
+
+textBox dialogueLoad();
+void drawDialogue(textBox * text, ALLEGRO_FONT* font, int textOption);
+
+#endif // !TEXTBOX_H
+
