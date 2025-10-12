2546c0210116fbb6be35479d7083a719b28900a8
Miguel Kato <miguelsinho2801@gmail.com>
2025-10-11 21:26:09 -0300

    novo gamerover.h

--- Milos-Grupo3/gameover.h
+++ Milos-Grupo3/gameover.h
@@ -2,28 +2,28 @@
 #define GAMEOVER_H
 
 #include <allegro5/allegro.h>
-#include <allegro5/allegro_font.h>
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 #include <stdbool.h>
-#include "level.h"  // para usar a struct Level
+#include "button.h"
+#include "gameConstants.h"
 
-typedef struct {
-    ALLEGRO_BITMAP* background;
-    ALLEGRO_BITMAP* gameSceneGray;
+typedef struct GameOver {
+    ALLEGRO_BITMAP* bg;
+    ALLEGRO_BITMAP* logo; // imagem "Você Perdeu"
     ALLEGRO_FONT* font;
-    ALLEGRO_MOUSE_STATE mouse;
-    ALLEGRO_RECT retryButton;
-    ALLEGRO_RECT exitButton;
-    bool running;
+    button retryButton;   // "Tela Inicial"
+    button exitButton;    // "Sair"
+    int selectedOption;   // 1 = Tela Inicial, 2 = Sair
+    bool active;          // indica se a tela está ativa
 } GameOver;
 
-// Cria o objeto GameOver
 GameOver createGameOver(ALLEGRO_FONT* font);
+void drawGameOver(GameOver* over, ALLEGRO_MOUSE_STATE* mouseState);
 
-// Mostra a tela de Game Over e retorna o novo Level quando o jogador escolher "Tentar novamente"
-level gameOver_Show(void);
-
-// Libera recursos
-void destroyGameOver(GameOver* over);
+extern GameOver gameOver_global;
+void showGameOver();
 
 #endif
+#pragma once
