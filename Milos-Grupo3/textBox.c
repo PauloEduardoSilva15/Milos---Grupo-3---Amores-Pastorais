2ded3381336c839198698db801f580cfcdcd6476
PauloEduardoSilva15 <pauloeduardob75@gmail.com>
2025-10-11 22:19:55 -0300

    Atualizando_Alfa0.0.8

---
+++ Milos-Grupo3/textBox.c
@@ -0,0 +1,37 @@
+#include "textBox.h"
+
+
+textBox dialogueLoad() {
+	textBox dialogue;
+	
+	dialogue.x = 0;
+	dialogue.y = 500;
+	dialogue.width = SCREEN_WIDTH;
+	dialogue.height = 100;
+	
+	dialogue.boxColor = DOR_COLOR;
+	return dialogue;
+
+}
+
+
+void drawDialogue(textBox * text, ALLEGRO_FONT* font, int textOption) {
+	text->font = font;
+	text->dialogueOption = textOption;
+	switch (textOption) {
+	case 0:
+		strcpy(text->personagem, "Homem Velho");
+		strcpy(text->texto, "Olá pessoa, linda, bonita. Maravilhosa Errante.");
+		break;
+	case 1:
+		strcpy(text->personagem, "Dirceu");
+		strcpy(text->texto, "Olá pessoa, linda, bonita. Maravilhosa. ssssss");
+	}
+
+	al_draw_filled_rectangle(text->x, text->y, text->x + text->width, text->y + text->height, text->boxColor);
+	al_draw_text(text->font, TEXT_COLOR, text->x + 25, text->y + 25, 0, text->personagem);
+	al_draw_text(text->font, TEXT_COLOR, text->x + 25, text->y + 35, 0, text->texto);
+	al_draw_text(text->font, TEXT_COLOR, text->width - 125, text->y + 70, 0, "Pressione T ->");
+	al_draw_text(text->font, TEXT_COLOR, text->x + 25, text->y + 70, 0, "Pressione R");
+}
+
