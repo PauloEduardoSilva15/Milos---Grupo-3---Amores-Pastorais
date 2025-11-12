204645ad75296ed8920895bc84afa36ead101c2b
PauloEduardoSilva15 <pauloeduardob75@gmail.com>
2025-11-12 11:46:59 -0300

    Betha1.4

--- Milos-Grupo3/gameConstants.h
+++ Milos-Grupo3/gameConstants.h
@@ -6,7 +6,7 @@
 
 
 #define TITLE "Pastoral Loves" // Titulo da Janela
-#define VERSION "Betha 1.3"// versão do jogo
+#define VERSION "Betha 1.4"// versão do jogo
 #define SCREEN_WIDTH 800 // Largura da Janela
 #define SCREEN_HEIGHT 600 // Altura da Janela
 #define FPS 60
